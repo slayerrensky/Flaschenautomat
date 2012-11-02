c4fce5879d43d4f7070460a1a7abfffd8606998a
PeteKaminski <p.kaminski00@gmail.com>
2012-11-02 09:38:01 +0100

    FlaschenTyp import in Fassade

---
+++ src/Fassade/Fassade.java
@@ -0,0 +1,77 @@
+package Fassade;
+import Automat.FlaschenType;
+
+/**
+ * @author Dennis
+ * @version 1.0
+ * @created 02-Nov-2012 9:30:04 AM
+ */
+public class Fassade {
+
+	public Fassade(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	/**
+	 * aktualisiertden Text des Displays, blockierend
+	 * 
+	 * @param Text
+	 */
+	public void aktualisiereDisplay(String Text){
+
+	}
+
+	/**
+	 * Wenn der Bon Button in der GUI gedrückt wurde
+	 */
+	public void bonButtonGedrueckt(){
+
+	}
+
+	/**
+	 * druckt, blockierend
+	 * 
+	 * @param Text
+	 */
+	public void drucken(String Text){
+
+	}
+
+	public void leuchteAN(){
+
+	}
+
+	public void leuchteAUS(){
+
+	}
+
+	/**
+	 * 
+	 * @param level
+	 * @param Text
+	 */
+	public void logText(int level, String Text){
+
+	}
+
+	/**
+	 * 
+	 * @param Type
+	 */
+	public void simFlascheEingelegt(FlaschenType Type){
+
+	}
+
+	public void troeteAN(){
+
+	}
+
+	public void troeteAUS(){
+
+	}
+
+}
